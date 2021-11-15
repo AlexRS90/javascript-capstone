0d662b0210cf7e6dc1a45591917840680028b468
Lino09 <onosmx@gmail.com>
2021-11-15 16:04:33 -0600

    setup jest

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import _ from 'lodash';
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
