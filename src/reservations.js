11b9df0b12ff45637060f6ddb91e787a82c5599f
Lino09 <onosmx@gmail.com>
2021-11-16 15:02:15 -0600

    Update function to be arrow function

--- src/reservations.js
+++ src/reservations.js
@@ -1,4 +1,4 @@
-export async function create(id) {
+export const create = async (id) => {
   const character = await fetch(`https://gateway.marvel.com/v1/public/characters/${id}?ts=1&hash=cef440fde913b8756e1db19d5f18cd9e&apikey=580e7d5773a2965034fd7fcd3cad5f53`).then((response) => response.json());
   const charData = character.data.results[0];
   const overlay = document.createElement('div');
@@ -24,7 +24,7 @@ export async function create(id) {
     document.body.removeChild(overlay);
   });
   document.body.appendChild(overlay);
-}
+};
 
 export function getReservation() {
 
