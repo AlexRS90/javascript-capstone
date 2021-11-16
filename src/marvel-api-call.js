6ceacdfa61aa368e1f4c8a30e7d03b95475d2d3a
AlexRS90 <aramosbengy@gmail.com>
2021-11-16 11:10:45 -0600

    Fix linters

--- src/marvel-api-call.js
+++ src/marvel-api-call.js
@@ -1,11 +1,11 @@
-import displaySHCards from "./index";
+import displaySHCards from './index';//eslint-disable-line
 
-const url ='https://gateway.marvel.com/v1/public/characters?ts=1&hash=cef440fde913b8756e1db19d5f18cd9e&apikey=580e7d5773a2965034fd7fcd3cad5f53';
+const url = 'https://gateway.marvel.com/v1/public/characters?ts=1&hash=cef440fde913b8756e1db19d5f18cd9e&apikey=580e7d5773a2965034fd7fcd3cad5f53';
 const apiCall = async () => {
   console.log("Entre a la funcion");
   const call = await fetch(url);
   const arr = await call.json();
-  console.log(arr.data.results)
+  console.log(arr.data.results);
   displaySHCards(arr.data.results);
 };
 
