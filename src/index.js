0f9f6017de8216dbc4ccc4e940ff1b9c89abe191
AlexRS90 <aramosbengy@gmail.com>
2021-11-16 11:00:11 -0600

    Add function to display dynamically heroes cards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './style.css';
+import apiCall from './marvel-api-call';
+
+let superHeroes = [];
+
+const displaySHCards = (arraySuperH) => {
+  superHeroes = arraySuperH;
+  let newCard = '';
+  superHeroes.forEach((heroes) => {
+    newCard += `<div class="card-container d-flex">
+    <img src="${heroes.thumbnail.path}/portrait_fantastic.jpg" alt="${heroes.name} pincture">
+    <div class="d-flex card-info">
+        <h3>${heroes.name}</h3>
+        <div>
+            <a href="#"><img src="" alt="Like super hero"></a>
+            <p>10 Likes</p>
+        </div>
+    </div>
+    <input type="button" class="btn-comments main-btn" value="Comments">
+    <input type="button" class="btn-reservation main-btn"value="Reservations">
+    </div>`;
+  });
+  document.querySelector('.super-heroes-container').innerHTML = newCard;
+}
+
+apiCall();
+
+export default displaySHCards;
