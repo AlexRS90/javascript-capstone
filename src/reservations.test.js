69368cffa5fe6a20259bb037b5e849cabd9cff66
Lino09 <onosmx@gmail.com>
2021-11-17 12:21:28 -0600

    refactor tests to use AAA pattern

--- src/reservations.test.js
+++ src/reservations.test.js
@@ -1,13 +1,19 @@
 import { countRsvtns } from './reservations.js';
 
-test('count total of reservations', () => {
-  expect(countRsvtns('000')).toBe('3 people is being rescued by this Hero in the next days');
+test('count multiple reservations', () => {
+  const reservations = [1, 2, 3];
+  const reservationsMessage = countRsvtns(reservations);
+  expect(reservationsMessage).toBe('3 people is being rescued by this Hero in the next days');
 });
 
-test('count total of reservations', () => {
-  expect(countRsvtns('')).toBe('0 people is being rescued by this Hero in the next days');
+test('count zero reservations', () => {
+  const reservations = [];
+  const reservationsMessage = countRsvtns(reservations);
+  expect(reservationsMessage).toBe('0 people is being rescued by this Hero in the next days');
 });
 
-test('count total of reservations', () => {
-  expect(countRsvtns('a')).toBe('1 person is being rescued by this Hero in the next days');
+test('count aunique reservation', () => {
+  const reservations = [1];
+  const reservationsMessage = countRsvtns(reservations);
+  expect(reservationsMessage).toBe('1 person is being rescued by this Hero in the next days');
 });
